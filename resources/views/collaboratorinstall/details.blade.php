b1817960b609606195bc0e2fc7b2ac052051f881
Hoang Nghia Nhu Y <nhuyhkc006@gmail.com>
2025-11-03 08:55:39 +0700

    Sửa lỗi khi trang thái đã hoàn thành thì mất nút cập nhật

--- resources/views/collaboratorinstall/details.blade.php
+++ resources/views/collaboratorinstall/details.blade.php
@@ -307,7 +307,7 @@
         <button id="btnPay" class="mt-2 ms-1 btn btn-outline-info fw-bold" data-action="payment">Đã thanh toán</button>
 
     @elseif($statusInstall == 2) {{-- Trạng thái 2: Đã Hoàn Thành --}}
-    {{-- Đã ẩn "Cập nhật" và "Hoàn thành", chỉ cho phép thanh toán --}}
+        <button id="btnUpdate" class="mt-2 btn btn-outline-primary fw-bold" data-action="update">Cập nhật</button>
         <button id="btnPay" class="mt-2 ms-1 btn btn-outline-info fw-bold" data-action="payment">Đã thanh toán</button>
 
     @endif 
