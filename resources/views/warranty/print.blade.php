abc55524b41883a786f0e32ab07a8d1e1a9cf5b9
LcNghia <nghia520414@gmail.com>
2025-11-22 13:42:43 +0700

    fix style phiếu in

--- resources/views/warranty/print.blade.php
+++ resources/views/warranty/print.blade.php
@@ -29,7 +29,7 @@
     <style>
         @page {
             size: A4;
-            margin-top: 5mm;
+            margin-top: 0;
             margin-bottom: 0;
             margin-left: 5mm;
             margin-right: 5mm;
@@ -95,6 +95,10 @@
         .qr-payment-top .qr-title {
             font-weight: bold;
         }
+
+        .content-section {
+            margin-top: -15px;
+        }
     </style>
 </head>
 
@@ -125,99 +129,100 @@
                 </td>
             </tr>
         </table>
+        <div class="content-section">
+            <div class="title">PHIẾU TIẾP NHẬN BẢO HÀNH</div>
+            @php
+                use Carbon\Carbon;
+                $now = Carbon::now('Asia/Ho_Chi_Minh');
+            @endphp
+            <div class="subtitle">In lúc {{ $now->format('H:i') }} ngày {{ $now->day }} tháng {{ $now->month }} năm
+                {{ $now->year }}
+            </div>
 
-        <div class="title">PHIẾU TIẾP NHẬN BẢO HÀNH</div>
-        @php
-            use Carbon\Carbon;
-            $now = Carbon::now('Asia/Ho_Chi_Minh');
-        @endphp
-        <div class="subtitle">In lúc {{ $now->format('H:i') }} ngày {{ $now->day }} tháng {{ $now->month }} năm
-            {{ $now->year }}
-        </div>
-
-        <table width="100%">
-            <tr>
-                <td width="50%"><strong>Mã số phiếu:</strong> {{ $data->id }} </td>
-                <td><strong>Serial:</strong> {{ strtoupper($data->serial_number) }}</td>
-            </tr>
-            <tr>
-                <td><strong>Họ và tên kh:</strong> {{ $data->full_name }}</td>
-                <td><strong>Ngày xuất kho:</strong> {{ \Carbon\Carbon::parse($data->shipment_date)->format('d/m/Y')}}
-                    ({{$strWar}})
-                </td>
-            </tr>
-            <tr>
-                <td><strong>Số điện thoại: </strong> {{ $data->phone_number }} </td>
-                @if($ctv!=null)<td><strong>Cộng tác viên: </strong> {{ $ctv['tenctv'] }} </td>@endif
-            </tr>
-            <tr>
+            <table width="100%">
+                <tr>
+                    <td width="50%"><strong>Mã số phiếu:</strong> {{ $data->id }} </td>
+                    <td><strong>Serial:</strong> {{ strtoupper($data->serial_number) }}</td>
+                </tr>
+                <tr>
+                    <td><strong>Họ và tên kh:</strong> {{ $data->full_name }}</td>
+                    <td><strong>Ngày xuất kho:</strong> {{ \Carbon\Carbon::parse($data->shipment_date)->format('d/m/Y')}}
+                        ({{$strWar}})
+                    </td>
+                </tr>
+                <tr>
+                    <td><strong>Số điện thoại: </strong> {{ $data->phone_number }} </td>
+                    @if($ctv!=null)<td><strong>Cộng tác viên: </strong> {{ $ctv['tenctv'] }} </td>@endif
+                </tr>
+                <tr>
+                    @if ($ctv != null)
+                    <td rowspan="2" valign="top"><strong>Địa chỉ KH:</strong> {{ $data->address }}</td>
+                        <td><strong>Số điện thoại CTV:</strong> {{ $ctv['sdt'] }}</td>
+                    @else
+                        <td colspan="2"><strong>Địa chỉ KH:</strong> {{ $data->address }}</td>
+                    @endif
+                </tr>
                 @if ($ctv != null)
-                <td rowspan="2" valign="top"><strong>Địa chỉ KH:</strong> {{ $data->address }}</td>
-                    <td><strong>Số điện thoại CTV:</strong> {{ $ctv['sdt'] }}</td>
-                @else
-                    <td colspan="2"><strong>Địa chỉ KH:</strong> {{ $data->address }}</td>
+                    <tr>
+                        <td><strong>Địa chỉ CTV:</strong> {{ $ctv['diachi'] }}</td>
+                    </tr>
                 @endif
-            </tr>
-            @if ($ctv != null)
                 <tr>
-                    <td><strong>Địa chỉ CTV:</strong> {{ $ctv['diachi'] }}</td>
+                    <td colspan="2"><strong>Sản phẩm:</strong> {{ $data->product }}</td>
                 </tr>
-            @endif
-            <tr>
-                <td colspan="2"><strong>Sản phẩm:</strong> {{ $data->product }}</td>
-            </tr>
-        </table>
+            </table>
 
-        <table width="100%" class="table_product" style="border-collapse: collapse; border: 1px solid black;">
-            <thead>
-                <tr>
-                    <th style="width: 12px;">STT</th>
-                    <th style="width: 250px;">Lỗi</th>
-                    <th>Linh kiện thay thế</th>
-                    <th>SL</th>
-                    <th>Đơn giá</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($items as $index => $item)
+            <table width="100%" class="table_product" style="border-collapse: collapse; border: 1px solid black;">
+                <thead>
                     <tr>
-                        <td style="text-align: center;">{{ $index + 1 }}</td>
-                        <td>{{ $item->error_type }}</td>
-                        <td>{{ $item->replacement }}</td>
-                        <td style="text-align: center;">{{ $item->quantity }}</td>
-                        <td style="text-align: center;">{{ number_format($item->unit_price, 0) }}</td>
+                        <th style="width: 12px;">STT</th>
+                        <th style="width: 250px;">Lỗi</th>
+                        <th>Linh kiện thay thế</th>
+                        <th>SL</th>
+                        <th>Đơn giá</th>
                     </tr>
-                @endforeach
-                @for ($i = 1; $i <= 6; $i++)
+                </thead>
+                <tbody>
+                    @foreach ($items as $index => $item)
+                        <tr>
+                            <td style="text-align: center;">{{ $index + 1 }}</td>
+                            <td>{{ $item->error_type }}</td>
+                            <td>{{ $item->replacement }}</td>
+                            <td style="text-align: center;">{{ $item->quantity }}</td>
+                            <td style="text-align: center;">{{ number_format($item->unit_price, 0) }}</td>
+                        </tr>
+                    @endforeach
+                    @for ($i = 1; $i <= 2; $i++)
+                        <tr>
+                            <td style="text-align: center;">{{ $items->count() + $i }}</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    @endfor
+                </tbody>
+                <tfoot>
                     <tr>
-                        <td style="text-align: center;">{{ $items->count() + $i }}</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td colspan="4" style="text-align: center;"><strong>TỔNG TIỀN</strong></td>
+                        <td style="text-align: center;"><strong>{{ number_format($total, 0) }}</strong></td>
                     </tr>
-                @endfor
-            </tbody>
-            <tfoot>
+                </tfoot>
+            </table>
+
+            <div><strong>Ghi chú:</strong></div>
+
+            <table class="chuky" width="100%">
                 <tr>
-                    <td colspan="4" style="text-align: center;"><strong>TỔNG TIỀN</strong></td>
-                    <td style="text-align: center;"><strong>{{ number_format($total, 0) }}</strong></td>
+                    <td><strong>Khách hàng</strong><br><i>(Ký, ghi rõ họ tên)</i></td>
+                    <td><strong>Nhân viên kỹ thuật</strong><br><i>(Ký, ghi rõ họ tên)</i></td>
                 </tr>
-            </tfoot>
-        </table>
-
-        <div><strong>Ghi chú:</strong></div>
-
-        <table class="chuky" width="100%">
-            <tr>
-                <td><strong>Khách hàng</strong><br><i>(Ký, ghi rõ họ tên)</i></td>
-                <td><strong>Nhân viên kỹ thuật</strong><br><i>(Ký, ghi rõ họ tên)</i></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><strong>{{ $data->staff_received }}</strong></td>
-            </tr>
-        </table>
+                <tr>
+                    <td></td>
+                    <td><strong>{{ $data->staff_received }}</strong></td>
+                </tr>
+            </table>
+        </div>
     </div>
 
 </body>
